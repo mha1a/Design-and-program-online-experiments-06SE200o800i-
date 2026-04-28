59bf04a4be31e5836ddd13e3e6c81bba07576ab6
Marceline Haller <marcelinedominiquekyemin.haller@uzh.ch>
2026-04-28 10:24:05 +0200

    mods

--- Session7/STARTER/canvas_jsPsych.js
+++ Session7/STARTER/canvas_jsPsych.js
@@ -15,139 +15,138 @@ const timeline = [];
 
 
 // instruction 
-const instruction = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: `<h2 id='instruction'> Please press the <strong>space bar</strong> as soon as the stimulus appears. </h2>
-               <p> press right arrow key to start! </p>
-    `,
-    choices: ['ArrowRight']
-}
-timeline.push(instruction);
+// const instruction = {
+//     type: jsPsychHtmlKeyboardResponse,
+//     stimulus: `<h2 id='instruction'> Please press the <strong>space bar</strong> as soon as the stimulus appears. </h2>
+//                <p> press right arrow key to start! </p>
+//     `,
+//     choices: ['ArrowRight']
+// }
+// timeline.push(instruction);
 
-// 7.2.2 make the trial_duration randomly selected from 1s, 2s or 3s
-// fixation 
-const fixation = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: "+",
-    choices: "NO_KEYS",
-    trial_duration: jsPsych.randomization.sampleWithoutReplacement([1000, 2000, 3000], 1)[0]
-}
-timeline.push(fixation);
+// // 7.2.2 make the trial_duration randomly selected from 1s, 2s or 3s
+// // fixation 
+// const fixation = {
+//     type: jsPsychHtmlKeyboardResponse,
+//     stimulus: "+",
+//     choices: "NO_KEYS",
+//     trial_duration: jsPsych.randomization.sampleWithoutReplacement([1000, 2000, 3000], 1)[0]
+// }
+// timeline.push(fixation);
 
 
-const drawBunnyfn = function (canvas){
+// const drawBunnyfn = function (canvas){
     
-    const context = canvas.getContext("2d");
+//     const context = canvas.getContext("2d");
    
-    // The following is the block of drawing a bunny
+//     // The following is the block of drawing a bunny
 
     
-       let x = canvas.width/2;
-       let y = canvas.height/2;
+//        let x = canvas.width/2;
+//        let y = canvas.height/2;
    
-       context.clearRect(0, 0, canvas.width, canvas.height);
+//        context.clearRect(0, 0, canvas.width, canvas.height);
    
-       // COLORS
-       const fur = "#F2F2F2";
-       const stroke = "#B0B0B0";
-       const blush = "#F8C8DC";
-       const nose = "#E89CA9";
+//        // COLORS
+//        const fur = "#F2F2F2";
+//        const stroke = "#B0B0B0";
+//        const blush = "#F8C8DC";
+//        const nose = "#E89CA9";
      
-       // EARS (Left & Right)
-       context.beginPath();
-       context.ellipse(x - 20, y - 50, 10, 30, 0, 0, 2 * Math.PI);
-       context.fillStyle = fur;
-       context.fill();
-       context.strokeStyle = stroke;
-       context.stroke();
+//        // EARS (Left & Right)
+//        context.beginPath();
+//        context.ellipse(x - 20, y - 50, 10, 30, 0, 0, 2 * Math.PI);
+//        context.fillStyle = fur;
+//        context.fill();
+//        context.strokeStyle = stroke;
+//        context.stroke();
      
-       context.beginPath();
-       context.ellipse(x + 20, y - 50, 10, 30, 0, 0, 2 * Math.PI);
-       context.fill();
-       context.stroke();
+//        context.beginPath();
+//        context.ellipse(x + 20, y - 50, 10, 30, 0, 0, 2 * Math.PI);
+//        context.fill();
+//        context.stroke();
      
-       // HEAD
-       context.beginPath();
-       context.arc(x, y, 40, 0, 2 * Math.PI);
-       context.fillStyle = fur;
-       context.fill();
-       context.stroke();
+//        // HEAD
+//        context.beginPath();
+//        context.arc(x, y, 40, 0, 2 * Math.PI);
+//        context.fillStyle = fur;
+//        context.fill();
+//        context.stroke();
      
-       // EYES
-       context.beginPath();
-       context.arc(x - 20, y - 10, 5, 0, 2 * Math.PI);
-       context.arc(x + 20, y - 10, 5, 0, 2 * Math.PI);
-       context.fillStyle = "#444";
-       context.fill();
+//        // EYES
+//        context.beginPath();
+//        context.arc(x - 20, y - 10, 5, 0, 2 * Math.PI);
+//        context.arc(x + 20, y - 10, 5, 0, 2 * Math.PI);
+//        context.fillStyle = "#444";
+//        context.fill();
      
-       // BLUSH (under eyes)
-       context.beginPath();
-       context.arc(x - 30, y + 10, 7, 0, 2 * Math.PI);
-       context.arc(x + 30, y + 10, 7, 0, 2 * Math.PI);
-       context.fillStyle = blush;
-       context.fill();
+//        // BLUSH (under eyes)
+//        context.beginPath();
+//        context.arc(x - 30, y + 10, 7, 0, 2 * Math.PI);
+//        context.arc(x + 30, y + 10, 7, 0, 2 * Math.PI);
+//        context.fillStyle = blush;
+//        context.fill();
      
-       // NOSE
-       context.beginPath();
-       context.moveTo(x - 5, y + 10);
-       context.lineTo(x + 5, y + 10);
-       context.lineTo(x, y + 18);
-       context.closePath();
-       context.fillStyle = nose;
-       context.fill();
+//        // NOSE
+//        context.beginPath();
+//        context.moveTo(x - 5, y + 10);
+//        context.lineTo(x + 5, y + 10);
+//        context.lineTo(x, y + 18);
+//        context.closePath();
+//        context.fillStyle = nose;
+//        context.fill();
 
-}
+// }
 
-const bunny_trial = {
-    type: jsPsychCanvasKeyboardResponse,
-    stimulus: drawBunnyfn,
-    choices: "NO_KEYS",
-    trial_duration: 2000
-}
-timeline.push(bunny_trial);
+// const bunny_trial = {
+//     type: jsPsychCanvasKeyboardResponse,
+//     stimulus: drawBunnyfn,
+//     choices: "NO_KEYS",
+//     trial_duration: 2000
+// }
+// timeline.push(bunny_trial);
 
-const end = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: "<h2>👍<h2>",
-    choices: "NO_KEYS",
-    trial_duration: 1000
-}
-timeline.push(end)
+// const end = {
+//     type: jsPsychHtmlKeyboardResponse,
+//     stimulus: "<h2>👍<h2>",
+//     choices: "NO_KEYS",
+//     trial_duration: 1000
+// }
+// timeline.push(end)
 
 
-const drawSmiley = function(canvas){
-    const ctx = canvas.getContext("2d");
-    let x = canvas.width/2;
-    let y = canvas.height/2;
+// const drawSmiley = function(canvas){
+//     const ctx = canvas.getContext("2d");
+//     let x = canvas.width/2;
+//     let y = canvas.height/2;
 
-    // face
-    ctx.beginPath();
-    ctx.arc(x, y, 50, 0, 2*Math.PI);
-    ctx.fillStyle = "yellow";
-    ctx.fill();
+//     // face
+//     ctx.beginPath();
+//     ctx.arc(x, y, 50, 0, 2*Math.PI);
+//     ctx.fillStyle = "yellow";
+//     ctx.fill();
 
-    // eyes
-    ctx.beginPath();
-    ctx.arc(x-20, y-10, 5, 0, 2*Math.PI);
-    ctx.arc(x+20, y-10, 5, 0, 2*Math.PI);
-    ctx.fillStyle = "black";
-    ctx.fill();
+//     // eyes
+//     ctx.beginPath();
+//     ctx.arc(x-20, y-10, 5, 0, 2*Math.PI);
+//     ctx.arc(x+20, y-10, 5, 0, 2*Math.PI);
+//     ctx.fillStyle = "black";
+//     ctx.fill();
 
-    // mouth
-    ctx.beginPath();
-    ctx.arc(x, y+10, 20, 0, Math.PI);
-    ctx.stroke();
-}
+//     // mouth
+//     ctx.beginPath();
+//     ctx.arc(x, y+10, 20, 0, Math.PI);
+//     ctx.stroke();
+// }
 
-const smiley_trial = {
-    type: jsPsychCanvasKeyboardResponse,
-    stimulus: drawSmiley,
-    choices: "NO_KEYS",
-    trial_duration: 2000
-};
+// const smiley_trial = {
+//     type: jsPsychCanvasKeyboardResponse,
+//     stimulus: drawSmiley,
+//     choices: "NO_KEYS",
+//     trial_duration: 2000
+// };
 
-timeline.push(smiley_trial);
-
+// timeline.push(smiley_trial);
 
 // step 4: run the timline
 jsPsych.run(timeline)
